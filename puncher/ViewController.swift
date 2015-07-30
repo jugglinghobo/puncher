bffec5f54f84223262bc21d3f75291e37649943c
Simon Kiener <jugglinghobo@gmail.com>
2015-07-30 03:50:09 +0200

    added basic time select and mailing functionality

--- puncher/ViewController.swift
+++ puncher/ViewController.swift
@@ -6,20 +6,56 @@
 //  Copyright (c) 2015 Simon Kiener. All rights reserved.
 //
 
+import Foundation
 import UIKit
+import MessageUI
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MFMailComposeViewControllerDelegate {
+    
+    @IBOutlet weak var dateTimeSelect: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func sendEmailButtonTapped(sender: AnyObject) {
+        let mailComposeViewController = configuredMailComposeViewController()
+        if MFMailComposeViewController.canSendMail() {
+            self.presentViewController(mailComposeViewController, animated: true, completion: nil)
+        } else {
+            self.showSendMailErrorAlert()
+        }
     }
-
-
+    
+    func configuredMailComposeViewController() -> MFMailComposeViewController {
+        
+        let punchDate = formatDate(dateTimeSelect.date)
+        
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self // Extremely important to set the --mailComposeDelegate-- property, NOT the --delegate-- property
+        
+        mailComposerVC.setToRecipients(["jugglinghobo@gmail.com"])
+        mailComposerVC.setSubject("Punch: \(punchDate)")
+        mailComposerVC.setMessageBody(punchDate, isHTML: false)
+        
+        return mailComposerVC
+    }
+    
+    func showSendMailErrorAlert() {
+        let sendMailErrorAlert = UIAlertView(title: "Could Not Send Email", message: "Your device could not send e-mail.  Please check e-mail configuration and try again.", delegate: self, cancelButtonTitle: "OK")
+        sendMailErrorAlert.show()
+    }
+    
+    
+    func formatDate(date: NSDate) -> String {
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "dd.MM.yy hh:mm"
+        var formattedDate = formatter.stringFromDate(dateTimeSelect.date)
+        return formattedDate
+    }
+    
+    // MARK: MFMailComposeViewControllerDelegate Method
+    func mailComposeController(controller: MFMailComposeViewController!, didFinishWithResult result: MFMailComposeResult, error: NSError!) {
+        controller.dismissViewControllerAnimated(true, completion: nil)
+    }
 }
-
