7c72ae1bb074f499c2ef4f02329e3823b0293831
Simon Kiener <jugglinghobo@gmail.com>
2015-07-30 02:11:34 +0200

    Initial Commit

---
+++ puncher/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  puncher
+//
+//  Created by Simon Kiener on 30.07.15.
+//  Copyright (c) 2015 Simon Kiener. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
